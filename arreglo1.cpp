d68514d78511f9e2dff0bb5d1edbe0f8d635db2f
wsalazarq <wsalazaq@gmail.com>
2024-07-01 11:00:16 -0500

    Arreglo_1: Calculo de la sumatoria y el porcentaje

--- arreglo1.cpp
+++ arreglo1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    int n, num;
+    int n, p, c = 0, s = 0;
     cout<<"Ingresa un numero: ";cin>>n;
+
+int A[n];
+    for (int i = 0; i < n; i++){
+        cout<<"Ingrese el elemento del arreglo "<<i<<": ";
+        cin>>A[i];
+        s = s + A[i];
+        if (A[i] == 0){
+            c++;
+        }
+    }
+
+    p = (c * 100) /n;
+    
+    cout<<"Sumatoria: "<<s<<endl;
+    cout<<"Porcentaje de ceros: "<<p<<"%"<<endl;
 
     return 0;
 }
