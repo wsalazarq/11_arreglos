6b78af93e0eb65416775e4b1424ced15ef073622
wsalazarq <wsalazaq@gmail.com>
2024-07-01 11:09:42 -0500

    Arreglo_2: Actualizar

--- arreglo2.cpp
+++ arreglo2.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
-    int n;
+    int n, par = 0, impar = 1;
     cout<<"Ingresa un numero: ";cin>>n;
 
+    int A[n];
+
+    for (int i = 0; i < n; i++){
+        cout<<"Ingrese el elemento del arreglo "<<i<<": ";
+        cin>>A[i];
+
+        if (A[i] % 2 == 0){
+            int P[par];
+            P[par] = A[i];
+            par++;
+        }else{
+            int Impar[impar];
+            Impar[impar] = A[i];
+            impar++;
+        }
+        
+    }
+    
     return 0;
 }
