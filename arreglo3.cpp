12133bec6d48da59ff25c3623d9358168c889cd3
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-07-02 18:43:23 -0500

    Arreglo_3: Calculo de la temperatura media de cada dia

--- arreglo3.cpp
+++ arreglo3.cpp
@@ -11,7 +11,11 @@ int main(){
         cout<<"Ingrese la temperatura maxima del dia "<< i + 1 <<" : ";cin>>tempMax[i];        
     }
 
-    
+    float temMedia[dias];
+    for (int i = 0; i < dias; i++){
+        temMedia[i] = (tempMin[i] + tempMax[i]) / 2;
+        cout<<"La temperatura media del dia "<< i + 1 <<" es: "<<temMedia[i]<<endl;
+    }
     
     return 0;
 }
