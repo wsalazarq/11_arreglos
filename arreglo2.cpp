48e2b3df214d93643d4a8561e35fd8f258e4be19
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-07-01 19:36:25 -0500

    Arreglo_2: Validar si los elementos son pares o impares

--- arreglo2.cpp
+++ arreglo2.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
-    int n;
+    int n, par = 0, impar = 0;
     cout<<"Ingresa un numero: ";cin>>n;
 
+    int A[n];
+    int P[n];
+    int Impar[n];
+
+    for (int i = 0; i < n; i++){
+        cout<<"Ingrese el elemento del arreglo "<<i<<": ";
+        cin>>A[i];
+
+        if (A[i] % 2 == 0){
+            P[par] = A[i];
+            par++;
+        }else{
+            Impar[impar] = A[i];
+            impar++;
+        }
+        
+    }
+    
     return 0;
 }
