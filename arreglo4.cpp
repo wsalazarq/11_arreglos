649279fbace2aa39e2d97f8d8b8081a662254167
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-07-03 20:44:30 -0500

    Arreglo_4:Crear la funcion llenarArreglo

--- arreglo4.cpp
+++ arreglo4.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+void llenarArreglo(int, int, int);
 
-    int n, A[n];
+int main() {
+    int n, mult, A[n];
 
     cout<<"Ingrese un valor: ";cin>>n;
+    cout<<"Ingrese un numero: ";cin>>mult;
 
-    
+    return 0;
+}
+
+void llenarArreglo(int A[], int n, int mult) {
+    for (int i = 0; i < n; i++) {
+        A[i] = mult * (i + 1);
+    }
 }
