db14485699950bc17a2ce2c0d98f6e085be0e1c9
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-07-04 19:23:12 -0500

    Arreglo_5: Implementar el arreglo FP

--- arreglo5.cpp
+++ arreglo5.cpp
@@ -6,5 +6,9 @@ int main(){
     int numFP;
     cout<<"Ingrese el numero de estudiantes de Fundamentos de Programación: ";cin>>numFP;
 
+    string FP[numFP];    
+    for (int i = 0; i < numFP; i++){
+        cout<<"Ingrese el estudiante numero "<<i + 1<<" : ";cin>>FP[i];
+    }
     return 0;
 }
