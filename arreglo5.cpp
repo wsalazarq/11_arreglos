bf91771d8074893ade3c444b70d3ee02619b6d0f
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-07-04 19:22:16 -0500

    Agregar arreglo5

---
+++ arreglo5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int numFP;
+    cout<<"Ingrese el numero de estudiantes de Fundamentos de Programación: ";cin>>numFP;
+
+    return 0;
+}
