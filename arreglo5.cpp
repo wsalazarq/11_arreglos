466bb9210b03543ecf2bd1c263d579a473fb923f
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-07-04 19:53:37 -0500

    Arreglo_5: Implementar el arreglo PG

--- arreglo5.cpp
+++ arreglo5.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 int main(){
 
-    int numFP;
+    int numFP, numPG;
     cout<<"Ingrese el numero de estudiantes de Fundamentos de Programación: ";cin>>numFP;
 
     string FP[numFP];    
     for (int i = 0; i < numFP; i++){
         cout<<"Ingrese el estudiante numero "<<i + 1<<" : ";cin>>FP[i];
     }
+
+    cout<<"Ingrese el numero de estudiantes de Programacion Grafica: ";cin>>numPG;
+
+    string PG[numPG];    
+    for (int i = 0; i < numPG; i++){
+        cout<<"Ingrese el estudiante numero "<<i + 1<<" : ";cin>>PG[i];
+    }
+    
     return 0;
 }
